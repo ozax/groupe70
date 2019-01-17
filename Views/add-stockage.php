48b5651dd71b1e13812f2c61e33388645c2942f7
Redha LALLAMI <xpeki123456@gmail.com>
2019-01-17 16:23:25 +0100

    Update add-stockage.php
    EPISODE 1 retour de valeurs de session

--- Views/add-stockage.php
+++ Views/add-stockage.php
@@ -19,24 +19,22 @@
                         <form action="" method="POST">
                             <div class="form-group">
                                 <label>denomination</label>
-                                <input type="text" name="denomination"  class="form-control" id="denomination" aria-describedby="Dénomination" placeholder="Enter Dénomination" required="true">
+                                <input  value="<?php if (isset(  $_SESSION['stockage']['denomination'])) echo  $_SESSION['stockage']['denomination'] ?> " type="text" name="denomination"  class="form-control" id="denomination" aria-describedby="Dénomination" placeholder="Enter Dénomination" required="true">
                             </div>
-
-
 
                             <div class="form-group">
                                 <label>Lieux</label>
-                                <input type="text" name="lieux"  class="form-control" id="lieux" aria-describedby="Lieux" placeholder="Enter Lieux" required="true">
+                                <input  value="<?php if (isset(  $_SESSION['stockage']['lieux'])) echo  $_SESSION['stockage']['lieux'] ?> " type="text" name="lieux"  class="form-control" id="lieux" aria-describedby="Lieux" placeholder="Enter Lieux" required="true">
                             </div>
 
                             <div class="form-group">
                                 <label>Quantité</label>
-                                <input type="text" name="quantite"  class="form-control" id="quantite" aria-describedby="Quantité" placeholder="Enter Quantité" required="true">
+                                <input  value="<?php if (isset(  $_SESSION['stockage']['quantite'])) echo  $_SESSION['stockage']['quantite'] ?> " type="text" name="quantite"  class="form-control" id="quantite" aria-describedby="Quantité" placeholder="Enter Quantité" required="true">
                             </div>
 
                             <div class="form-group">
                                 <label>Entreprise</label>
-                                <input type="text" name="entreprise"  class="form-control" id="entreprise" aria-describedby="Entreprise" placeholder="Enter Entreprise" required="true">
+                                <input  value="<?php if (isset(  $_SESSION['stockage']['entreprise'])) echo  $_SESSION['stockage']['entreprise'] ?> " type="text" name="entreprise"  class="form-control" id="entreprise" aria-describedby="Entreprise" placeholder="Enter Entreprise" required="true">
                             </div>
 
 
@@ -44,9 +42,9 @@
                                 <label for="chantier" >Chantier</label>
 
                                 <select class="form-control" id="chantier" name="chantier">
-                                    <?PHP foreach ($chantier as $chantier): ?>
 
-                                        <option value="<?=$chantier["nomChantier"]?>"> <?=$chantier["nomChantier"]?>
+                                    <?PHP foreach ($chantier as $chantier): ?>
+                                        <option value="<?php if (isset( $_SESSION['stockage']['Chantier'])) echo  $_SESSION['stockage']['Chantier']; else echo $chantier["nomChantier"] ?>"> <?php echo $chantier["nomChantier"] ?>
                                         </option>
                                     <?php endforeach;?>
 
@@ -62,3 +60,4 @@
         </div>
     </div>
 </div>
+<?php unset($_SESSION['stockage']);?>
